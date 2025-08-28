97f986f61aef6c2871a4e233c39a4a443ced7b65
husnialfarisal-tech <huhsnialfarisal@gmail.com>
2025-08-28 09:57:17 +0900

    Upload Pertama Project

---
+++ bin/project_husni.dart
@@ -0,0 +1,5 @@
+import 'package:project_husni/project_husni.dart' as project_husni;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_husni.calculate()}!');
+}
---
+++ test/project_husni_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_husni/project_husni.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
