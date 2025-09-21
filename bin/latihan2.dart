84a70865391d8eb74328174945570509584d9b49
husnialfarisal-tech <huhsnialfarisal@gmail.com>
2025-09-21 10:01:38 +0900

    latihan 1 pertemuan 2

---
+++ bin/latihan2.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Menyimpan panjang dan lebar dalam variabel bertipe double
+  double panjang = 10;
+  double lebar = 5;
+
+  // Menghitung luas dan keliling
+  double luas = panjang * lebar;
+  double keliling = 2 * (panjang + lebar);
+
+  // Menampilkan hasil
+  print("=== Perhitungan Persegi Panjang ===");
+  print("Panjang  : $panjang");
+  print("Lebar    : $lebar");
+  print("Luas     : $luas");
+  print("Keliling : $keliling");
+}
